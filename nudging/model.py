8e1e768e2322f65df535d4aea14510a349c9b60b
Colin Cotter <colin.cotter@imperial.ac.uk>
2022-09-07 11:15:15 +0100

    tidied up and created observation operator

--- nudging/model.py
+++ nudging/model.py
@@ -1,10 +1,10 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
-class model(object, metaclass=ABCMeta):
+class base_model(object, metaclass=ABCMeta):
     def __init__(self):
         pass
 
     @abstractmethod
-    def run(nsteps, W, X0, X1):
+    def run(self, nsteps, W, X0, X1):
         """
         nsteps - number of timesteps
         W - an nsteps x k array containing random numbers
@@ -12,4 +12,14 @@ class model(object, metaclass=ABCMeta):
         X1- a Firedrake Function to copy the result into
         """
         pass
+
+    def obs(self, X0):
+        """
+        Observation operator
+
+        X0 - a Firedrake Function containing the initial condition
+
+        returns
+
+        y - a k-dimensional numpy array of the observations
     
