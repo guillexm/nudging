2cd2624484c1e426d21fa84dad86a926bd1bee7c
maneeshrs51 <maneeshpapu@gmail.com>
2022-10-19 17:48:47 +0100

    Removed uncommented lines form script files

--- Example/assimilation_ch_srcipt.py
+++ Example/assimilation_ch_srcipt.py
@@ -3,27 +3,14 @@ from nudging import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 """ read obs from saved file 
-    Do assimilation step
+    Do assimilation step for tempering and jittering steps 
 """
-
-
-
-
-
-# For mesh generation
 model = Camsholm(100)
 x, = SpatialCoordinate(model.mesh) 
 
-# bootstrap filter
-#bfilter = bootstrap_filter(5, (5, 4))
 bfilter = jittertemp_filter(5, (5, 4), n_temp=5, n_jitt=5, rho=0.46)
-nensemble = 10
-#print(bfilter.nsteps)
-#bfilter.setup(nensemble, model)
-
-# initialise ensemble
+nensemble = 40
 bfilter.setup(nensemble, model)
 
 dx0 = Constant(0.)
@@ -40,64 +27,34 @@ for i in range(nensemble):
     u0_exp = a*0.2*2/(exp(x-403./15. + dx0) + exp(-x+403./15. + dx0)) \
            + b*0.5*2/(exp(x-203./15. + dx1)+exp(-x+203./15. + dx1))
 
-
-    # used perturbed solution as initial data for update  ensemble[i] via run/observation method
     _, u = bfilter.ensemble[i].split()
     u.interpolate(u0_exp)  
 
 def log_likelihood(dY):
     return np.dot(dY, dY)/0.1**2
-
-#print([len(a) for a in bfilter.ensemble])
+    
 #Load data
-N_obs = 5
-# true data  load
+N_obs = 25
 y_exact = np.load('y_true.npy')
-plt.plot(y_exact[:,10], 'r-', label='y_true')
-# synthetic data load
+plt.plot(y_exact[:,10], 'r-', label='Y_true')
+
 y = np.load('y_obs.npy') 
-#plt.plot(y[:,10], 'b-', label='y_obs')
 
-# plt.show()
-
-#y_obs_rows = iter(y_obsdata)
-#print(y[0,:])
-y_e = np.zeros((N_obs, nensemble, y.shape[1])) # y_ensemble
+y_e = np.zeros((N_obs, nensemble, y.shape[1]))
 y_e_mean = np.zeros((N_obs, nensemble))
-#y_e_var = np.zeros((N_obs, nensemble))
 
 print(y_e.shape)
 
 # do assimiliation step
 for k in range(N_obs):
     bfilter.assimilation_step(y[k,:], log_likelihood)
-    #print(bfilter.ess)
-    #print(bfilter.e_weight, bfilter.e_oldweight)
-    #print(bfilter.e_weight)
-    
-    #print(bfilter.e_s)
-    #print("New copies: ", bfilter.e_copies)
-    #print("L_new: ", bfilter.e_L)
     for e in range(nensemble):
         y_e[k,e,:] = model.obs(bfilter.ensemble[e])
-        #y_e_mean = np.mean(y_e[k,e,10])
 
 y_e_mean = np.mean(y_e[:,:,10], axis=1)
-#y_e_var = np.var(y_e[:,:,10])
 
-#print(y_e_mean)
 plt.plot(y_e[:,:,10], 'y-')
-plt.plot(y_e_mean, 'g-', label='y_ensemble_mean')
-#plt.plot(y_e_var, 'r-', label='y_ensemble_std')
+plt.plot(y_e_mean, 'g-', label='Y_ensemble_mean')
 plt.legend()
 plt.title('Ensemble prediction with N_ensemble = ' +str(nensemble))
 plt.show()
-
-
-
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.plot_surface(k, e, y_e[:,:,20])
-# plt.show()
-
-
--- Example/data_ch_script.py
+++ Example/data_ch_script.py
@@ -18,28 +18,23 @@ x, = SpatialCoordinate(model.mesh)
 u0.interpolate(0.2*2/(exp(x-403./15.) + exp(-x+403./15.)) + 0.5*2/(exp(x-203./15.)+exp(-x+203./15.)))
 
 dt = 0.01
-nsteps = 5 # number of time steps
-N_obs = 5  # number of observation for different time t_1----t_Nobs
-DT = nsteps*dt # time steps for observation data
-nobs_step = nsteps*N_obs # no of obs setp in terms of actual steps
+nsteps = 25 
+N_obs = 5  
+DT = nsteps*dt 
+nobs_step = nsteps*N_obs 
 
-# for writing the output at observation points
-#y_true = File('y_truth.pvd')
 y_true = model.obs(X_truth)
 y_obs_full = np.zeros((N_obs, np.size(y_true)))
 y_true_full = np.zeros((N_obs, np.size(y_true)))
 
 for i in range(N_obs):
-    #i_N = i*nsteps
-    W_truth = np.random.randn(nsteps, 4) # 4 is the problem noise  parameter
+    W_truth = np.random.randn(nsteps, 4)
     model.run(nsteps, W_truth, X_truth, X_truth)
     y_true = model.obs(X_truth)
 
     y_true_full[i,:] = y_true
     y_true_data = np.save("y_true.npy", y_true_full)
 
-
-    #np.random.seed(100) # for fixing the randomness
     y_noise = np.random.normal(0.0, 0.05, 40)  
 
     y_obs = y_true + y_noise
@@ -47,7 +42,3 @@ for i in range(N_obs):
     y_obs_full[i,:] = y_obs 
 
     y_obsdata = np.save("y_obs.npy", y_obs_full)
-    
-
-
-
