69f63f8a0b9f64a5d794620f5d45696d62178e5c
Maneesh Kumar Singh <maneesh.sr51@gmail.com>
2023-01-13 14:22:27 +0000

    made a simple model to check parallel resampling

---
+++ nudging/models/sim_model.py
@@ -0,0 +1,25 @@
+from firedrake import *
+from firedrake.petsc import PETSc
+from pyop2.mpi import MPI
+from nudging.model import *
+import numpy as np
+
+
+class SimModel(base_model):
+
+    def __init__(self):
+        pass
+
+    def setup(self, comm):
+        self.mesh = UnitSquareMesh(2,2, comm = comm)
+        self.V = FunctionSpace(self.mesh, "CG", 1)
+
+    def run(self, X):
+        X.assign(self.ensemble_rank)
+
+    def obs(self):
+        return np.random.normal(0,1)
+
+    def allocate(self):
+        return Function(self.V)
+
---
+++ tests/test_parallel_resample.py
@@ -0,0 +1,32 @@
+from firedrake import *
+from pyop2.mpi import MPI
+from nudging import *
+from nudging.models.sim_model import SimModel
+import numpy as np
+
+
+nensemble = [2,1,2,2,1]
+
+model = SimModel()
+
+y_true = model.obs()
+y_noise = np.random.normal(0.0, 0.01)
+
+y = y_true + y_noise
+
+def log_likelihood(dY):
+    return np.dot(dY, dY)/2
+
+
+
+simfilter = sim_filter()
+simfilter.setup(nensemble, model)
+model.ensemble_rank = simfilter.ensemble_rank
+
+simfilter.assimilation_step(y, log_likelihood)
+
+
+
+
+
+
