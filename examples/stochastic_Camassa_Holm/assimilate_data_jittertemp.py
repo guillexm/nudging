55c53cac4ead834b46b33d082ffd9fef2951c303
Colin Cotter <colin.cotter@imperial.ac.uk>
2023-03-01 08:54:11 +0000

    tidying up assimilate_data_jittertemp.py

--- examples/stochastic_Camassa_Holm/assimilate_data_jittertemp.py
+++ examples/stochastic_Camassa_Holm/assimilate_data_jittertemp.py
@@ -56,36 +56,32 @@ y_e_mean = np.zeros((N_obs, sum(nensemble)))
 
 yVOM = Function(model.VVOM)
 
+# prepare shared arrays for data
+y_e_list_arr = []
+for m in range(y.shape[1]):        
+    y_e_shared_arr = SharedArray(partition=nensemble, 
+                                 comm=jtfilter.subcommunicators.ensemble_comm)
+    y_e_list_arr.append(y_e_shared_arr)
+
+ys = y.shape
+if jtfilter.ensemble_rank == 0:
+    y_e = np.zeros(np.sum(nensemble), ys[0], ys[1])
+
 # do assimiliation step
-y_e_lst = []
 for k in range(N_obs):
     PETSc.Sys.Print("Step", k)
     yVOM.dat.data[:] = y[k, :]
     jtfilter.assimilation_step(yVOM, log_likelihood)
-
-    y_e_list_arr = []
-    for m in range(y.shape[1]):
         
-        y_e_shared_arr = SharedArray(partition=nensemble, 
-                                      comm=jtfilter.subcommunicators.ensemble_comm)
-        for i in range(nensemble[jtfilter.ensemble_rank]):
-            model.w0.assign(jtfilter.ensemble[i][0])
-            y_e_shared_arr.dlocal[i] = model.obs().dat.data[m]
-            y_e_shared_arr.synchronise()
-        y_e_list_arr.append(y_e_shared_arr.data())
-    y_e_lst.append(y_e_list_arr)
+    for i in range(nensemble[jtfilter.ensemble_rank]):
+        model.w0.assign(jtfilter.ensemble[i][0])
+        obsdata = model.obs().dat.data[:]
+        for m in range(y.shape[1]):
+            y_e_list_arr[m].dlocal[i] = obsdata[m]
 
-y_e_fin_arr = np.array(y_e_lst)
-y_e = np.transpose(y_e_fin_arr, (0,2,1))
-y_e_mean = np.mean(y_e[:,:,20], axis=1)
-print(y_e.shape)
+    for m in range(y.shape[1]):
+        y_e_shared_arr[m].synchronise()
+        if jtfilter.ensemble_rank == 0:
+            y_e[:, k, m] = y_e_shared_arr[m].data())
 
-if jtfilter.ensemble_rank == 0:
-   
-    plt.plot(y_exact[:,20], 'r-', label='y_obs')
-    #plt.plot(y[:,20], 'b-', label='y_obs_+_noise')
-    plt.plot(y_e[:,:,20], 'y-')
-    plt.plot(y_e_mean, 'g-', label='y_ensemble_mean')
-    plt.title('Ensemble prediction with N_ensemble = ' +str(sum(nensemble)))
-    plt.legend()
-    plt.show()
+np.save("ensemble_simulated_obs.npy", y_e)
