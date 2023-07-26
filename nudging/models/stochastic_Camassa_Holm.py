9810b30fe2de72d08ac89bfa8a7931ebcbf18933
Colin Cotter <colin.cotter@imperial.ac.uk>
2023-07-26 16:43:30 +0100

    one noise variable per step

--- nudging/models/stochastic_Camassa_Holm.py
+++ nudging/models/stochastic_Camassa_Holm.py
@@ -60,20 +60,15 @@ class Camsholm(base_model):
         self.fx4.interpolate(0.1*sin(4.*pi*self.x/8.))
 
         # with added term
-        self.R = FunctionSpace(self.mesh, "R", 0)
-        self.dW = []
-        for i in range(self.nsteps):
-            subdW = []
-            for j in range(4):
-                subdW.append(Function(self.R))
-            self.dW.append(subdW)
+        self.noise_space = VectorFunctionSpace(self.mesh, "R", 0, dim=4)
+        self.dW = Function(self.noise_space)
+        self.Ln = (
+            self.fx1*self.dW1+
+            self.fx2*self.dW2+
+            self.fx3*self.dW3+
+            self.fx4*self.dW4
+            )
 
-        self.dW1 = Function(self.R)
-        self.dW2 = Function(self.R)
-        self.dW3 = Function(self.R)
-        self.dW4 = Function(self.R)
-        self.Ln = self.fx1*self.dW1+self.fx2*self.dW2+self.fx3*self.dW3+self.fx4*self.dW4
-        
         # finite element linear functional 
         Dt = Constant(self.dt)
         self.mh = 0.5*(self.m1 + self.m0)
@@ -111,10 +106,7 @@ class Camsholm(base_model):
         self.w0.assign(self.X[0])
         self.msolve.solve()
         for step in range(self.nsteps):
-            self.dW1.assign(self.X[4*step+1])
-            self.dW2.assign(self.X[4*step+2])
-            self.dW3.assign(self.X[4*step+3])
-            self.dW4.assign(self.X[4*step+4])
+            self.dW.assign(self.X[step+1]
 
             self.usolver.solve()
             self.w0.assign(self.w1)
@@ -136,22 +128,23 @@ class Camsholm(base_model):
         return Y
 
 
-    def allocate(self):
+    def allocate(self, noise=False, lambdas=False):
         particle = [Function(self.W)]
-        for i in range(self.nsteps):
-            for j in range(4):
-                dW = Function(self.R)
-                dW.assign(self.rg.normal(self.R, 0., 1.0))
-                particle.append(dW) 
-        return particle 
-
+        if noise:
+            for i in range(self.nsteps):
+                dW = self.rg.normal(self.noise_space, 0., 1.0)
+                particle.append(dW)
+        if lambdas:
+            for i in range(self.nsteps):
+                dW = self.rg.normal(self.noise_space, 0., 1.0)
+                particle.append(dW)
+        return particle
 
     def randomize(self, X, c1=0, c2=1, gscale=None, g=None):
         rg = self.rg
         count = 0
         for i in range(self.nsteps):
-            for j in range(4):
-                count += 1
-                X[count].assign(c1*X[count] + c2*rg.normal(self.R, 0., 1.0))
-                if g:
-                    X[count] += gscale*g[count]
+            count += 1
+            X[count].assign(c1*X[count] + c2*rg.normal(self.noise_space, 0., 1.0))
+            if g:
+                X[count] += gscale*g[count]
