56b4832a230280042b879b9d3de60e1bf7c6158e
Colin Cotter <colin.cotter@imperial.ac.uk>
2023-12-19 14:54:29 +0000

    toml

--- nudging/resampling.py
+++ nudging/resampling.py
@@ -1,9 +1,7 @@
 import numpy as np
-from abc import ABCMeta, abstractmethod, abstractproperty
 from functools import cached_property
-from firedrake import *
-from nudging import *
-from firedrake.petsc import PETSc
+import firedrake as fd
+
 
 class residual_resampling(object):
     def __init__(self, seed=34523, residual=False):
@@ -13,32 +11,31 @@ class residual_resampling(object):
 
     @cached_property
     def rg(self):
-        pcg = PCG64(seed=self.seed)
-        return RandomGenerator(pcg)
+        pcg = fd.PCG64(seed=self.seed)
+        return fd.RandomGenerator(pcg)
 
     def resample(self, weights, model):
         """
         :arg weights : a numpy array of normalised weights, size N
-        
+
         returns
         :arg s: an array of integers, size N. X_i will be replaced
         with X_{s_i}.
         """
         if not self.initialised:
             self.initialised = True
-            self.R = FunctionSpace(model.mesh, "R", 0)
+            self.R = fd.FunctionSpace(model.mesh, "R", 0)
         N = weights.size
 
         if self.residual:
-            copies = np.array(np.floor(weights*N), dtype=int) 
+            copies = np.array(np.floor(weights*N), dtype=int)
             L = N - np.sum(copies)
             residual_weights = N*weights - copies
             residual_weights /= np.sum(residual_weights)
-        
+
             for i in range(L):
                 u = self.rg.uniform(self.R, 0., 1.0)
-                u0 =  u.dat.data[:]
-
+                u0 = u.dat.data[:]
                 cs = np.cumsum(residual_weights)
                 istar = -1
                 while cs[istar+1] < u0:
@@ -51,8 +48,8 @@ class residual_resampling(object):
                 for j in range(copies[i]):
                     s[count] = i
                     count += 1
-    
-        else: # systemitic resampling
+
+        else:  # systematic resampling
             cumsum_weight = np.cumsum(weights)
             ensembl_pos = (self.rg.random() + np.arange(N))/N
             s = np.zeros(N, dtype=int)
@@ -62,6 +59,5 @@ class residual_resampling(object):
                     s[i] = j
                     i += 1
                 else:
-                    j +=1
+                    j += 1
         return s
-        
